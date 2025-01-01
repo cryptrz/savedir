6d105de50daf942f2071e2c49e57ae6df75081ce
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-01-01 19:39:25 +0100

    Update savedir.sh
    Added an option for changing the destination folder even in the interactive mode

--- savedir.sh
+++ savedir.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 BACKUP_DIRECTORY="/home/$USER/backups"
 
 if [ $# -eq 0 ]; then
         echo "Type the path to directory to copy (Full path): "
         read SOURCE_DIRECTORY
+
+        echo "Do you want to keep the default backup folder /home/$USER/backups? Y/n (or another key to cancel) "
+        read DESTINATION_CHOICE
+
+        if [ "$DESTINATION_CHOICE" = "n"  ] || [ "$DESTINATION_CHOICE" = "N" ]; then
+                echo "Enter the full path for the destination folder: "
+                read DESTINATION_PATH
+                BACKUP_DIRECTORY=$DESTINATION_PATH
+        elif [ "$DESTINATION_CHOICE" = "y"  ] || [ "$DESTINATION_CHOICE" = "Y" ]; then
+                echo "The destination folder has not been modified"
+        else
+                echo "Closing the program"
+                exit 1
+        fi
 elif [ $# -eq 1 ]; then
         SOURCE_DIRECTORY=$1
 elif [ $# -eq 2 ]; then
@@ -16,12 +30,12 @@ else
         or\n 
         'sh savedir.sh <source path>'\n
         or\n 
-        'sh savedir.sh <source path> <destination path'>"
+        'sh savedir.sh <source path> <destination path>'"
         exit 1
 fi
 
 if [ ! -d "$SOURCE_DIRECTORY" ]; then
-        echo "Error: The directory $SOURCE_DIRECTORY does not exist !"
+        echo "Error: The directory $SOURCE_DIRECTORY does not exist!"
         exit 1
 fi
 
@@ -29,10 +43,10 @@ if [ ! -d "$BACKUP_DIRECTORY" ]; then
         mkdir -p "$BACKUP_DIRECTORY"
 fi
 
-cp -R "$SOURCE_DIRECTORY/" $BACKUP_DIRECTORY
+cp -R "$SOURCE_DIRECTORY/" "$BACKUP_DIRECTORY"
 
 if [ $? -eq 0 ]; then
-        echo "Backup successfull! The copy is stored in $BACKUP_DIRECTORY$(basename $SOURCE_DIRECTORY)"
+        echo "Backup successful! The copy is stored in $BACKUP_DIRECTORY/$(basename $SOURCE_DIRECTORY)"
 else
         echo "Backup failed!"
 fi
